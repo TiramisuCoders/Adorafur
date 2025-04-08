12cfc93cf987fc343378481034b01cb7f73637fe
judiebae <judeemmanuel.flores.cics@ust.edu.ph>
2025-04-09 01:13:36 +0800

    fix admin bookings css

--- admin/admin_bookings.php
+++ admin/admin_bookings.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin-css/admin_header1.css">
+    <link rel="stylesheet" href="admin-css/admin_header01.css">
     <link rel="stylesheet" href="admin-css/admin_bookings.css">
     <link rel="icon" type="image/png" href="admin-pics/adorafur-logo.png">
     <title>Admin Bookings</title>
